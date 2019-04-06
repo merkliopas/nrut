40d642b0074ff5153f680f82067b5e0d8189cc6f
merkliopas <sarunasmerkliopas@gmail.com>
2019-04-06 22:14:38 +0300

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -9,24 +9,6 @@ LD_LIBRARY_PATH=\$HOME/lib:\$LD_LIBRARY_PATH
 C_INCLUDE_PATH=\$HOME/include
 CPLUS_INCLUDE_PATH=\$HOME/include
 
-export NVM_DIR="\$HOME/.nvm"
-NVM_DIR="\$HOME/.nvm"
-[ -s "\$NVM_DIR/nvm.sh" ] && . "\$NVM_DIR/nvm.sh"
-[ -s "\$NVM_DIR/bash_completion" ] && . "\$NVM_DIR/bash_completion"
-
-if [ -d "~/.nvm" ]; then
-	source "\$NVM_DIR/nvm.sh"
-fi
-EOT
-
-#get nodejs and npm, required for color scheme and other applets
-cd ~/
-git clone https://github.com/creationix/nvm.git ~/.nvm 
-cd ~/.nvm 
-latesttag=$(git describe --tags)
-git checkout ${latesttag}
-cd $DIR
-
 #install ncurses
 git clone https://github.com/gittup/ncurses temp
 cd temp
