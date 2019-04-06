63b95e85713e7a21fff89b92d399bc013107bc61
merkliopas <sarunasmerkliopas@gmail.com>
2019-04-06 22:48:11 +0300

    Update configs.sh

--- configs.sh
+++ configs.sh
@@ -5,6 +5,8 @@ cp configs/.bashrc ~/.bashrc
 cp configs/.zshrc ~/.zshrc
 cp configs/.vimrc ~/.vimrc
 cp configs/terminator ~/.config/terminator/config
+cp configs/dircolorslight ~/dircolorslight
+cp configs/dircolorsdark ~/.dir_colors
 
 cd ~
 git clone https://github.com/merkliopas/tmux-config.git
