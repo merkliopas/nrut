8078ba2ac803fa0dab36dec97964622b39027a46
Sarunas Merkliopas <sarunas.merkliopas@euromonitor.com>
2019-04-07 10:12:15 +0300

    added vim solarization

--- configs.sh
+++ configs.sh
@@ -3,10 +3,15 @@ set -e
 
 cp configs/.bashrc ~/.bashrc
 cp configs/.zshrc ~/.zshrc
+
 cp configs/.vimrc ~/.vimrc
+cp -R configs/.vim/colors ~/.vim/
+cp -R configs/.vim/vim-colors-solarized ~/.vim/bundle
+
 cp configs/terminator ~/.config/terminator/config
 cp configs/dircolorslight ~/dircolorslight
 cp configs/dircolorsdark ~/.dir_colors
+
 
 cd ~
 git clone https://github.com/merkliopas/tmux-config.git
